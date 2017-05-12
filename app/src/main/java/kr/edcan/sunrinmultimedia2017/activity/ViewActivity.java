4ad9922fa530b328c7a119248a4e1591185e1555
Junseok Oh <kotohana5706@edcan.kr>
2017-05-13 00:35:26 +0900

    [Activity] Add Activities

---
+++ app/src/main/java/kr/edcan/sunrinmultimedia2017/activity/ViewActivity.java
@@ -0,0 +1,15 @@
+package kr.edcan.sunrinmultimedia2017.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.edcan.sunrinmultimedia2017.R;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
