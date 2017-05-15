e5cbda0d7f5863db6ec0554fde2a4860771511d0
Junseok Oh <kotohana5706@edcan.kr>
2017-05-16 04:33:13 +0900

    [Utils] Use ExhibitContentSingleTon

---
+++ app/src/main/java/kr/edcan/sunrinmultimedia2017/models/ExhibitContentSingleTon.java
@@ -0,0 +1,9 @@
+package kr.edcan.sunrinmultimedia2017.models;
+
+/**
+ * Created by fluor on 2017-05-16.
+ */
+
+public class ExhibitContentSingleTon {
+    public static String currentSelectedProjectId = "";
+}
